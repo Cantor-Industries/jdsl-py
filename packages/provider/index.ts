fc1227620b2dac092d3ac59388d1879307cc8703
skyguard <skyguard.africa@gmail.com>
2026-06-02 19:40:57 +0300

    consolidated languageModel functions

--- packages/provider/index.ts
+++ packages/provider/index.ts
@@ -1,32 +1,40 @@
-import { Effect } from "effect";
+import { Data, Effect } from "effect";
 import { generateText as generateTextAiSdk, streamText as streamTextAiSdk } from "ai";
 import { ContextWindow } from "src/context/context.ts";
 import { AiModel } from "./src/aiModel.ts";
+import { AiProvider } from "src/providers.ts";
 
+export class LanguageModelError extends Data.TaggedError("LanguageModelError")<{ msg: string }> { }
 export class LanguageModel extends Effect.Service<LanguageModel>()(
     "LanguageModel",
     {
         effect: Effect.gen(function* () {
             const aiModel = yield* AiModel;
             const contextWindow = yield* ContextWindow;
+            const provider = yield* AiProvider;
 
             const generateText = (prompt: string) => Effect.gen(function* () {
                 const model = yield* aiModel.getModel();
-                contextWindow.push({message: {role: "user", content: prompt}});
+                contextWindow.push({ message: { role: "user", content: prompt } });
                 const window = yield* contextWindow.join();
-
-                const fromAsync = () => Effect.tryPromise(async () => {
-                    const response = await generateTextAiSdk({
-                        model: model,
-                        system: window.systemInstructions,
-                        messages: window.messages,
-                        maxRetries: 5,
-                    })
-                    return response
+                // TO-DO: Add specific error msg or add cause property
+                const fromAsync = () => Effect.tryPromise({
+                    try: async () => {
+                        const response = await generateTextAiSdk({
+                            model: model,
+                            system: window.systemInstructions,
+                            messages: window.messages,
+                            maxRetries: 5,
+                        })
+                        return response
+                    },
+                    catch() {
+                        return new LanguageModelError({ msg: "generateText failed with unknownException"});
+                    },
                 })
-                const {content, text, reasoning, reasoningText, finishReason, usage, totalUsage} = yield* fromAsync();
+                const { content, text, reasoning, reasoningText, finishReason, usage, totalUsage } = yield* fromAsync();
                 yield* contextWindow.pop();
-                return {content, text, reasoning, reasoningText, finishReason, usage, totalUsage};
+                return { content, text, reasoning, reasoningText, finishReason, usage, totalUsage };
             })
 
             const streamText = (text: string) => Effect.gen(function* () {
@@ -45,7 +53,11 @@ export class LanguageModel extends Effect.Service<LanguageModel>()(
                 return yield* fromAsync(text)
             })
 
-            return { generateText, streamText } as const
-        })
+            const chooseProvider = provider.chooseProvider;
+            const chooseModel = provider.chooseModel;
+
+            return { generateText, streamText, chooseModel, chooseProvider } as const
+        }),
+        dependencies: [AiModel.Default, AiProvider.Default],
     }
 ) { }
